1407825ea06ec1d85a1db3020c92ddb8edfbc9c4
elyssontanaka <elysson.tanaka@gmail.com>
2022-07-01 17:35:35 -0300

    adding app.js file

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const info = document.querySelector(".info-container");
-const msg = document.createElement("p");
-msg.appendChild(document.createTextNode("Hello!"));
+// CONSTANTS
 const button = document.querySelector(".submit-btn");
-msg.className = "verification-msg";
+const input = document.getElementById("email");
+const invalidMsg = document.querySelector(".verification-txt");
 
-button.addEventListener("click", verify);
+// CLICK SUBMIT BUTTON EVENT
+button.addEventListener("click", validateEmail);
 
-function verify () {
-    info.appendChild(msg);
+// VALIDATE EMAIL FUNCTION
+function validateEmail () {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value) === true) {
+        invalidMsg.style.display = "none";
+        input.style.border = "black";
+        return true;
+    } else {
+        invalidMsg.style.display = "inline-block";
+        input.style.border = "1px solid red";
+        input.style.color = "black";
+    }
 }
